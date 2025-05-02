4cb929e3bed081cfa72ca459675acf0c26f4a490
Yarolsav <denisaroslsav34@gmail.com>
2025-05-02 21:38:14 +0300

    feat:create best selling section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import MenuProducts from '@/components/MenuProducts.vue'
+import BestSelling from '@/components/BestSelling.vue'
 </script>
 
 <template>
   <MenuProducts />
+  <BestSelling />
 </template>
